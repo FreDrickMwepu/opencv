c92bb19538850560bf512aa4a3f69e32dfed565a
Fredrick Mwepu <fredrickmwepu22@gmail.com>
2023-12-26 02:03:27 +0200

    Fourth commit

--- Chapter7.py
+++ Chapter7.py
@@ -1,6 +1,9 @@
 import cv2
+
 import numpy as np
 
+def empty(a):
+    pass
 
 def stackImages(scale,imgArray):
     rows = len(imgArray)
@@ -33,18 +36,44 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-img = cv2.imread('Resources/lena.jpeg')
+path = "Resources/car.jpeg"
 
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgStack = stackImages(0.5, ([img, imgGray, img],[img, imgGray, img]))
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 19, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 110, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 240, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 150, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
-# imgHor = np.hstack((img,img))
-# imgVer = np.vstack((img,img))
-#
-# cv2.imshow("Horizontal",imgHor)
-# cv2.imshow("Vertical",imgVer)
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min, h_max, s_max, s_min, v_max, v_min)
 
-cv2.imshow("ImageStack", imgStack)
-cv2.imshow("ImageGrey", imgGray)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask = mask)
 
-cv2.waitKey(0)
+    # cv2.imshow("Original", img)
+    # cv2.imshow("HSV", imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imgResult)
+
+    imgStack = stackImages(0.6, ([img, imgHSV], [mask, imgResult]))
+    cv2.imshow("Stacked Images", imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+
