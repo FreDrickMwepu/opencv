77c7a3115782b3db9006bd52f5163dd860016902
Fredrick Mwepu <fredrickmwepu22@gmail.com>
2024-02-24 21:09:45 +0200

    made some changes to the code base of chapter 9

--- Chapter9.py
+++ Chapter9.py
@@ -26,20 +26,5 @@ cv2.imshow("Result", img_lena)
 
 # Wait for any key press to close the displayed image
 cv2.waitKey(0)
-=======
-import cv2
 
-faceCascade = cv2.CascadeClassifier("Resources/haarcascades/haarcascade_frontalface_default.xml")
-img_lena = cv2.imread('Resources/lena.jpeg')
-imgGray = cv2.cvtColor(img_lena, cv2.COLOR_BGR2GRAY)
-
-faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
-
-// Hello world
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(img_lena, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-cv2.imshow("Result", img_lena)
-cv2.waitKey(0)
 >>>>>>> origin/main
